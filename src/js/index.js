d7911243ba1d19a937224bd501de5be795b135fd
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2019-05-11 15:54:52 +0700

    закончен блок со слайдером

--- src/js/index.js
+++ src/js/index.js
@@ -7,4 +7,12 @@ $( document ).ready(function() {
 		dots: true,
 		dotsClass: 'reviews-slider__dots'
 	});
+
+	$('.banner-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		dots: false
+	});
 });
