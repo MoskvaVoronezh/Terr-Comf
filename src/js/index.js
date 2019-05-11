620a078f7509dff216fad3c83794304714e6647a
Roman_Moskvin <moskvin.roman2018@yandex.ru>
2019-05-11 11:43:05 +0700

    Слайдер нижний

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+
+$( document ).ready(function() {
+   $('.reviews-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1, 
+		dots: true,
+		dotsClass: 'reviews-slider__dots'
+	});
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 //= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js
+//= ../../node_modules/slick-carousel/slick/slick.min.js
+//= ../js/index.js
 
 
 
