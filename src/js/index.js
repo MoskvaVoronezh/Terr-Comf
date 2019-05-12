5786d9cc779487e90a21a7ddf4b3b7ddfffd5aa8
Moskva_Voronezh <roman_moskvin_97@mail.ru>
2019-05-13 01:57:57 +0700

    завершена верстка сайта

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,15 @@ $( document ).ready(function() {
 		slidesToShow: 1,
 		slidesToScroll: 1, 
 		dots: true,
-		dotsClass: 'reviews-slider__dots'
+		dotsClass: 'reviews-slider__dots',
+		responsive: [
+			{
+				breakpoint: 900,
+				settings: {
+					arrows: false
+				}
+			}
+		]
 	});
 
 	$('.banner-slider').slick({
@@ -13,11 +21,17 @@ $( document ).ready(function() {
 		slidesToShow: 1,
 		slidesToScroll: 1,
 		arrows: false,
-		dots: false
+		dots: false,
+		adaptiveHeight: true
 	});
 
 	$(".fancybox").fancybox({
 		openEffect: 'elastic',
 		closeEffect: 'elastic'
+	});
+
+	$(".blog-list__link").on("click", function (e) {
+		e.preventDefault();
+		$(this).toggleClass("blog-link_active");
 	});
 });
